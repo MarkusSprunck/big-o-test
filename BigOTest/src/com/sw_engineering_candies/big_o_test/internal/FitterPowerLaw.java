0e8858092938d493535dcb74baed416962af9a5e
spm2ot <sprunck.markus@gmail.com>
2013-11-05 11:51:22 +0100

    Remove not needed stuff

--- BigOTest/src/com/sw_engineering_candies/big_o_test/internal/FitterPowerLaw.java
+++ BigOTest/src/com/sw_engineering_candies/big_o_test/internal/FitterPowerLaw.java
@@ -33,10 +33,6 @@ package com.sw_engineering_candies.big_o_test.internal;
 
 import java.util.Map;
 
-import org.apache.commons.math3.analysis.ParametricUnivariateFunction;
-import org.apache.commons.math3.fitting.CurveFitter;
-import org.apache.commons.math3.optim.nonlinear.vector.jacobian.LevenbergMarquardtOptimizer;
-
 public class FitterPowerLaw extends FitterAbstractBase {
 
 	/**
@@ -83,45 +79,6 @@ public class FitterPowerLaw extends FitterAbstractBase {
 		final double a = Math.exp((sum3 - b * sum2) / n);
 		super.coefficients.add(0, a);
 		super.coefficients.add(1, b);
-
-		final LevenbergMarquardtOptimizer optimizer = new LevenbergMarquardtOptimizer();
-		final CurveFitter<ParametricUnivariateFunction> curveFitter = new CurveFitter<ParametricUnivariateFunction>(
-				optimizer);
-		for (int pointIndex = 1; pointIndex <= super.xValues.size(); pointIndex++) {
-			final double x = super.xValues.get(pointIndex);
-			final double y = super.yValues.get(pointIndex);
-			curveFitter.addObservedPoint(x, y);
-		}
-		final ParametricUnivariateFunction f = new ParametricUnivariateFunction() {
-			@Override
-			public double value(double x, double... parameters) {
-				final double a = parameters[0];
-				final double b = parameters[1];
-				return a * Math.pow(x, b);
-			}
-
-			@Override
-			public double[] gradient(double x, double... parameters) {
-
-				final double b = parameters[1];
-				final double[] gradients = new double[2];
-
-				// derivative with respect to a
-				gradients[0] = 1;
-
-				// derivative with respect to b
-				gradients[1] = Math.pow(x, b) * Math.log(x);
-
-				return gradients;
-
-			}
-		};
-
-		final double[] initialGuess = new double[] { coefficients.get(0), coefficients.get(1) };
-		final double[] estimatedParameters = curveFitter.fit(f, initialGuess);
-
-		super.coefficients.add(0, estimatedParameters[0]);
-		super.coefficients.add(1, estimatedParameters[1]);
 	}
 
 	@Override
