e7f619f4252f8462a3d6b1d3663496e06f5aca1d
MarkusSprunck <sprunck.markus@gmail.com>
2015-11-07 15:41:14 +0100

    Refactoring improve performance

--- BigOTest-Demo/src/com/sw_engineering_candies/tests/PrimeNumbersLambdaTest.java
+++ BigOTest-Demo/src/com/sw_engineering_candies/tests/PrimeNumbersLambdaTest.java
@@ -1,5 +1,7 @@
 package com.sw_engineering_candies.tests;
 
+import static org.junit.Assert.*;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,24 +24,30 @@ public class PrimeNumbersLambdaTest {
     * filters all prime numbers in a list of integer values and creates a string with all results
     */
    public String filterPrimeNumbersLambdaWrapper(@BigOParameter List<Integer> values) {
-      return values.stream() //
-            .parallel() //
-            .filter(x -> isPrimeLambda(x)) //
-            .map(Integer::valueOf) //
-            .map(s -> s.toString()) //
-            .collect(Collectors.joining(" "));
+      return values.parallelStream() // execute in parallel mode
+            .filter(number -> number % 2 != 0 || number < 3) // remove all which can't be primes
+            .filter(number -> isPrimeLambda(number)) // check if the number is a prime
+            .map(number -> Integer.toString(number)) // create string
+            .collect(Collectors.joining(" ")); // join all strings
    }
 
    /**
     * prime number test
     */
    public static boolean isPrimeLambda(int n) {
-      if (n <= 1) {
-         return false;
-      } else if (n == 2) {
-         return true;
-      }
-      return n >= 2 && IntStream.rangeClosed(2, (int) (Math.sqrt(n))).allMatch((d) -> n % d != 0);
+      return n >= 2 && IntStream.rangeClosed(2, (int) (Math.sqrt(n))).allMatch((d) -> n % d != 0) || n == 2;
+   }
+
+   @Test
+   public void filterPrimeNumbersLambdaWrapper_SmallList_DetectAllPrimeNumbers() {
+      // ARRANGE
+      final List<Integer> values = createSortInput(22);
+
+      // ACT
+      String result = filterPrimeNumbersLambdaWrapper(values);
+
+      // ASSERT
+      assertEquals("2 3 5 7 11 13 17 19", result);
    }
 
    @Test
@@ -69,7 +77,7 @@ public class PrimeNumbersLambdaTest {
       System.out.println(BigOReports.getDataReport(data));
 
       // ASSERT
-      BigOAssert.assertLinear(boa, "filterPrimeNumbersLambdaWrapper");
+      BigOAssert.assertPolynomialDegree(boa, "filterPrimeNumbersLambdaWrapper", 1.05, 0.5);
    }
 
    private static List<Integer> createSortInput(Integer size) {
